08b115364efea6d934cb3b43d64f2001a3e469ab
MishoGobejishvili <gobejishvilimisho@gmail.com>
2024-05-14 21:36:27 +0400

    Made some minor changes

---
+++ Part_2/Func.py
@@ -0,0 +1,124 @@
+import random
+
+
+def card_chooser(players):
+    cards = [
+        "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
+        "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
+        "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
+        "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC"
+    ]
+
+    random.shuffle(cards)
+
+    all_player = []
+    for _ in range(players):
+        hand = []
+        for _ in range(5):
+            card = cards.pop()
+            hand.append(card)
+        all_player.append(hand)
+
+    return all_player
+
+
+# -----------------------------------------------------------------------------------------------
+
+def card_replacer(players_cards):
+    cards = [
+        "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
+        "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
+        "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
+        "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC"
+    ]
+
+    for value in players_cards.values():
+        for item in value:
+            cards.remove(item)
+
+    shuffle = random.choice(cards)
+
+    return shuffle
+
+
+# -----------------------------------------------------------------------------------------------
+
+def points_counter(player_cards):
+    points = 0
+
+    for item in player_cards:
+        if len(item) == 3:
+            points += 10
+        if item[0].isdigit():
+            points += int(item[0])
+        if item[0] == "J":
+            points += 11
+        if item[0] == "Q":
+            points += 12
+        if item[0] == "K":
+            points += 13
+        if item[0] == "A":
+            points += 20
+
+    return points
+
+
+# -----------------------------------------------------------------------------------------------
+
+def colours_counter(player_cards):
+    colours = {"S": 0, "H": 0, "D": 0, "C": 0}
+
+    for card in player_cards:
+        suit = card[-1]
+        colours[suit] += 1
+
+    max_key = max(colours, key=lambda x: colours[x])
+
+    return colours[max_key]
+
+
+# -----------------------------------------------------------------------------------------------
+
+def value_counter(player_cards):
+    values = {"2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0,
+              "8": 0, "9": 0, "10": 0, "J": 0, "Q": 0, "K": 0, "A": 0}
+
+    for item in player_cards:
+        if len(item) == 3:
+            key = item[:2]
+            values[key] += 1
+        else:
+            key = item[0]
+            values[key] += 1
+
+    max_key = max(values, key=lambda x: values[x])
+
+    return values[max_key]
+
+# -----------------------------------------------------------------------------------------------
+
+def find_max_by_method(dealt_cards: dict, method) -> bool:
+    player_points = dict()
+    max_points_counter = 0
+    max_points_name = str()
+
+    for name, cards in dealt_cards.items():
+        player_points[name] = method(cards)
+
+    max_points = max(player_points.values())
+
+    for player in player_points:
+        if player_points[player] == max_points:
+            max_points_name = player
+            max_points_counter += 1
+
+    if max_points_counter == 1:
+        print(f"Player {max_points_name} won.")
+        return True
+
+# -----------------------------------------------------------------------------------------------
+
+def decide_winner(dealt_cards: dict):
+    find_max_by_method(dealt_cards, points_counter)
+    find_max_by_method(dealt_cards, colours_counter)
+    find_max_by_method(dealt_cards, value_counter)
---
+++ Part_2/Game.py
@@ -0,0 +1,32 @@
+from Func import *
+
+NUMBER_OF_PLAYERS = 3
+dealt_cards = {}
+
+deal_cards = card_chooser(NUMBER_OF_PLAYERS)
+
+for player in range(NUMBER_OF_PLAYERS):
+    name = input("Please enter your name: ")
+    dealt_cards[name] = deal_cards[player]
+
+for name, cards in dealt_cards.items():
+
+    while True:
+        hand = ', '.join(cards)
+        user_answer = input(f"{name}, you are holding {hand}. \nif you want to change the card, choose the appropriate index from 1 to 5 inclusive, or enter 'NO': ")
+        if user_answer == "NO":
+            break
+        elif user_answer in "12345":
+            index = int(user_answer) - 1
+            dealt_cards[name][index] = card_replacer(dealt_cards)
+            break
+        else:
+            print("Please enter a correct answer!")
+
+print()
+
+decide_winner(dealt_cards)
+
+for name, cards in dealt_cards.items():
+    hand = ', '.join(cards)
+    print(f"{name}, you are holding {hand}. Your total score is {points_counter(cards)}, The maximum number of colors is {colours_counter(cards)}, You have {value_counter(cards)} of a kind")
