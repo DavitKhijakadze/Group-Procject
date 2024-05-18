7e09271f9f573b6f80be33685db14062afb4e818
DavitKhijakadze <dato.khijakadze0@gmail.com>
2024-05-18 22:02:30 +0400

    add final version

--- Func.py
+++ Func.py
@@ -1,7 +1,7 @@
 import random
 
 
-def card_chooser(players):
+def card_chooser(players: int):
     cards = [
         "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
         "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
@@ -24,7 +24,7 @@ def card_chooser(players):
 
 # -----------------------------------------------------------------------------------------------
 
-def card_replacer(players_cards):
+def card_replacer(players_cards: dict):
     cards = [
         "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
         "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
@@ -36,14 +36,14 @@ def card_replacer(players_cards):
         for item in value:
             cards.remove(item)
 
-    shuffle = random.choice(cards)
+    new_card = random.choice(cards)
 
-    return shuffle
-
+    return new_card
 
 # -----------------------------------------------------------------------------------------------
 
-def points_counter(player_cards):
+def points_counter(player_cards: list):
+
     points = 0
 
     for item in player_cards:
@@ -62,10 +62,9 @@ def points_counter(player_cards):
 
     return points
 
-
 # -----------------------------------------------------------------------------------------------
 
-def colours_counter(player_cards):
+def colours_counter(player_cards: list):
     colours = {"S": 0, "H": 0, "D": 0, "C": 0}
 
     for card in player_cards:
@@ -76,10 +75,9 @@ def colours_counter(player_cards):
 
     return colours[max_key]
 
-
 # -----------------------------------------------------------------------------------------------
 
-def value_counter(player_cards):
+def value_counter(player_cards: list):
     values = {"2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0,
               "8": 0, "9": 0, "10": 0, "J": 0, "Q": 0, "K": 0, "A": 0}
 
@@ -95,58 +93,33 @@ def value_counter(player_cards):
 
     return values[max_key]
 
+#--------------------------------------------------------------------
 
-# -----------------------------------------------------------------------------------------------
+def reveal_loser(players: dict):
+    points = {}
+    colours = {}
+    values = {}   
+    
+    for player, cards in players.items():
+        points[player] = points_counter(cards)
+        colours[player] = colours_counter(cards)
+        values[player] = value_counter(cards)
+        
+    for loser in [points, colours, values]:
+        min_value = min(loser.values())
+        count = 0
+        min_key = ""
 
-def find_min_max(player_points: dict) -> tuple:
-    max_points_counter = 0
-    max_points_name = str()
-    min_points_name = str()
-    max_points = max(player_points.values())
-    min_points = min(player_points.values())
+        for key, value in loser.items():
+            if value == min_value:
+                count += 1
+                min_key = key
 
-    for player in player_points:
-        if player_points[player] == max_points:
-            max_points_name = player
-            max_points_counter += 1
-        if player_points[player] == min_points:
-            min_points_name = player
-    return max_points_counter, max_points_name, max_points, min_points_name, min_points
-
-
-# -----------------------------------------------------------------------------------------------
-
-def won_by(method) -> str:
-    if method == points_counter:
-        return "Points"
-    elif method == colours_counter:
-        return "Colours"
-    elif method == value_counter:
-        return "Cards"
-
-# -----------------------------------------------------------------------------------------------
-
-def find_max_by_method(dealt_cards: dict, method) -> bool:
-    player_points = dict()
-    player_won_by = won_by(method)
-
-    for name, cards in dealt_cards.items():
-        player_points[name] = method(cards)
-
-    max_points_counter, max_points_name, max_points, min_points_name, min_points = find_min_max(player_points)
-
-    if max_points_counter == 1:
-        cards = dealt_cards[max_points_name]
-        print(f"Player {max_points_name} won by {player_won_by} with {cards}.")
-        return True
-    elif min_points_name != str() and min_points != max_points:
-        dealt_cards.pop(min_points_name)
-
-
-# -----------------------------------------------------------------------------------------------
-
-def decide_winner(dealt_cards: dict):
-    if find_max_by_method(dealt_cards, points_counter) or find_max_by_method(dealt_cards, colours_counter) or find_max_by_method(dealt_cards, value_counter):
-        return
-    else:
-        print("Match ended with draw.")
+        if count == 1:
+            return min_key
+        else:
+            continue
+        
+    return "Draw"
+        
+#--------------------------------------------------------------------
--- Game.py
+++ Game.py
@@ -1,44 +1,61 @@
 from Func import *
 
-NUMBER_OF_PLAYERS = 3
-PLAYERS = set()
-
+number_of_players = int(input("Enter the number of players: "))
+names_of_players = []
 dealt_cards = {}
 
-deal_cards = card_chooser(NUMBER_OF_PLAYERS)
-
-player_index = 0
-
-while len(PLAYERS) != 3:
-    name = input("Please enter your name: ")
-    if not (name in PLAYERS):
-        dealt_cards[name] = deal_cards[player_index]
-        PLAYERS.add(name)
-        player_index += 1
-    else:
-        print(f"Player by name {name} already exists.")
-
-print()
-
-for name, cards in dealt_cards.items():
-
-    while True:
-        hand = ', '.join(cards)
-        user_answer = input(f"{name}, you are holding {hand}. \nif you want to change the card, choose the appropriate index from 1 to 5 inclusive, or enter 'NO': ")
-        print()
-        if user_answer == "NO":
-            break
-        elif user_answer in "12345":
-            index = int(user_answer) - 1
-            dealt_cards[name][index] = card_replacer(dealt_cards)
-            break
+if len(dealt_cards) == 0:       
+    while len(names_of_players) != number_of_players:
+        name = input("Please enter your name: ")
+        if name not in names_of_players:
+            names_of_players.append(name)
         else:
-            print("Please enter a correct answer!")
+            print(f"Player by name {name} already exists.")
 
-for name, cards in dealt_cards.items():
-    hand = ', '.join(cards)
-    print(f"{name}, you are holding {hand}. Your total score is {points_counter(cards)}, The maximum number of colors is {colours_counter(cards)}, You have {value_counter(cards)} of a kind")
+while True:
+    
+    all_card_in_game = card_chooser(number_of_players)
+    
+    card_index = 0
+    for player in names_of_players:
+        dealt_cards[player] = all_card_in_game[card_index]
+        card_index += 1
 
-print()
+    print()
 
-decide_winner(dealt_cards)
+    for name, cards in dealt_cards.items():
+
+        while True:
+            hand = ', '.join(cards)
+            user_answer = input(f"{name}, you are holding {hand}. \nif you want to change the card, choose the appropriate index from 1 to 5 inclusive, or enter 'NO': ")
+            print()
+            if user_answer == "NO":
+                break
+            elif user_answer in "12345":
+                index = int(user_answer) - 1
+                dealt_cards[name][index] = card_replacer(dealt_cards)
+                break
+            else:
+                print("Please enter a correct answer!")
+
+    for name, cards in dealt_cards.items():
+        hand = ', '.join(cards)
+        print(f"{name}, you are holding {hand}. Your total score is {points_counter(cards)}, The maximum number of colors is {colours_counter(cards)}, You have {value_counter(cards)} of a kind")
+
+    print()
+
+    loser = reveal_loser(dealt_cards)
+
+    if loser == "Draw":
+        print("it is draw, keep playing")
+        exit()
+    elif loser in dealt_cards.keys():
+        print(f"{loser} left the game")
+        print()
+        print("-" * 100)
+        del dealt_cards[loser]
+        names_of_players.remove(loser)
+    
+    if len(dealt_cards.keys()) == 1:
+        print(f"Congratulations {names_of_players[0]}, you won!")
+        break
